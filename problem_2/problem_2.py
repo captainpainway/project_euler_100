32109635b1a591267b8f0402d79507bd1f9b1976
Mary Knize <maryknize@gmail.com>
2020-11-22 15:45:45 -0500

    Update to python problem 3

--- problem_2/problem_2.py
+++ problem_2/problem_2.py
@@ -11,3 +11,29 @@ def fib(n):
     return sum(acc)
 
 print(fib(4000000))
+
+def fib2(n):
+    arr = [1, 2]
+    while True:
+        if arr[-1] + arr[-2] > n:
+            break
+        arr.append(arr[-1] + arr[-2])
+    return sum(filter(lambda x: x % 2 == 0 ,arr))
+
+print(fib2(4000000))
+
+# Only adding every third number, which is even
+def fib3(n):
+    acc = 0
+    a = 1
+    b = 1
+    while True:
+        c = a + b
+        if c > n:
+            break
+        acc += c
+        a = c + b
+        b = a + c
+    return acc
+
+print(fib3(4000000))
